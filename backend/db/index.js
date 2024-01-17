e8fded321de50343035c23bc370ec8d998dd0831
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2024-01-18 00:37:20 +0530

    fixed code

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const mongodbURL = require("../config/index");
+const { mongodbURL } = require("../config/index");
 mongoose.connect(mongodbURL);
 
 const todoSchema = new mongoose.Schema({
     title: String,
     description: String,
-    completed: Boolean
+    completed: {type: Boolean, default: false}
 })
 
 const userSchema = new mongoose.Schema({
